1481ec44dcb66c8e52cad564b229650d0859e859
Marvin Roman <marvin@colab.coop>
2023-05-12 22:05:20 -0700

    feat: added fetching events using multiple API KEYS

--- src/fetch-events.py
+++ src/fetch-events.py
@@ -1,4 +1,4 @@
-# pylint: disable=invalid-name,import-error,consider-using-f-string,missing-module-docstring,line-too-long
+# pylint: disable=invalid-name,import-error,consider-using-f-string,missing-module-docstring,line-too-long,redefined-outer-name
 
 import json
 import logging
@@ -7,105 +7,85 @@ import re
 import requests
 import arrow
 
+# Set up logging
+logging.basicConfig(
+    format='%(asctime)s %(message)s',
+    filename='public/fetch.log',
+    level=logging.DEBUG
+)
+
 # get settings
-API_KEY = os.getenv("API_KEY")
+API_KEYS = os.getenv("API_KEYS").split(',')
 DOMAIN = os.getenv("DOMAIN")
 EVENTS_URI = os.getenv("EVENTS_URI")
-FUTURE_DAYS = int(os.getenv("FUTURE_DAYS"))
 PAST_DAYS = int(os.getenv("PAST_DAYS"))
+URL = f'https://{DOMAIN}/api/v2/{EVENTS_URI}'
 
-# Setup logging
-logging.basicConfig(format='%(asctime)s %(message)s',
-                    filename='public/fetch.log', level=logging.DEBUG)
+def get_event_embed(event, headers):
+    """Fetches the embed code for the event."""
 
-# setup API url
-headers = {"OSDI-API-Token": API_KEY}
-url = "https://{domain}/api/v2/{uri}".format(domain=DOMAIN, uri=EVENTS_URI)
-logging.debug("url: %s", url)
-
-# initiate variables
-current_page = 1
-total_pages = 2
-output = []
-
-def getEmbed(event_object):
-    """ fetches the embed code for the event
-
-    Args:
-        event_object (Object): the event currently being processed
-
-    Returns:
-        Object: object that includes the parsed src & id
-    """
-
-    if bool(event_object.get("_links")) is False:
+    links = event.get('_links', {})
+    embed_url = links.get('action_network:embed', {}).get('href')
+    if not embed_url:
         return {}
-
-    links = event_object.get("_links")
-
-    if bool(links.get("action_network:embed")) is False:
-        return {}
-
-    embed_url = links.get("action_network:embed")
 
     # make API call
-    embed_response = requests.get(embed_url.get("href"), headers=headers, timeout=10)
+    response = requests.get(embed_url, headers=headers, timeout=10)
     # convert embed_response to JSON object
-    json_embed_response = embed_response.json()
-
-    embed_full_no_styles = json_embed_response.get("embed_full_no_styles")
-
+    json_response = response.json()
+    embed_full_no_styles = json_response.get("embed_full_no_styles", "")
     match = re.search("src='([^']+)'.*id='([^']+)'", embed_full_no_styles)
 
     if match:
-        return {
-            "src": match.group(1), "id": match.group(2)
-        }
+        return {'src': match.group(1), 'id': match.group(2)}
     else:
         return {}
 
+output = []
 
-# loop through pages
-while current_page <= total_pages:
-    # set current page to fetch
-    params = {"page": current_page}
-    # make API call
-    response = requests.get(url, headers=headers, params=params, timeout=10)
-    # convert response to JSON object
-    json_response = response.json()
+# Loop through API keys
+for api_key in (key for key in API_KEYS):
+    current_page = 1
+    total_pages = 2
+    headers = {'OSDI-API-Token': api_key}
 
-    # get paging information
-    current_page = json_response["page"]
-    total_pages = json_response["total_pages"]
-    logging.debug("current_page: %s, total_pages: %s",
-                  current_page, total_pages)
+    # loop through pages
+    while current_page <= total_pages:
+        params = {
+            "page": current_page,
+            "filter": f"start_date gt '{arrow.get().shift(days=-PAST_DAYS).format('YYYY-MM-DD')}'"
+        }
+        response = requests.get(URL, headers=headers, params=params, timeout=10)
+        json_response = response.json()
 
-    # loop through returned events
-    for event in json_response["_embedded"]["osdi:events"]:
+        # get paging information
+        current_page = json_response.get('page', current_page)
+        total_pages = json_response.get('total_pages', total_pages)
+        logging.debug('current_page: %s, total_pages: %s', current_page, total_pages)
 
-        # setup calendar boundaries
-        first_available = arrow.get().shift(days=-PAST_DAYS)
-        last_available = arrow.get().shift(days=FUTURE_DAYS)
+        # loop through returned events
+        for event in json_response.get('_embedded', {}).get('osdi:events', []):
 
-        # if the event is between the calendar boundaries then add to output
-        if arrow.get(event.get("start_date")).is_between(first_available, last_available) and event.get("status") == "confirmed":
-            output.append({
-                "name": event.get("title"), 
-                "start": arrow.get(event.get("start_date")).format("YYYY-MM-DD HH:mm"), 
-                "embed": getEmbed(event), 
-                "end": arrow.get(event.get("end_date")).format("YYYY-MM-DD HH:mm") if event.get("end_date") else "", 
-                "description": event.get("description", ""), 
-                "featured_image_url": event.get("featured_image_url", ""),
-                "instructions": event.get("instructions", ""),
-                "location": event.get("location"),
-                "uid": event.get("identifiers"),
-                "link": event.get("browser_url"), 
-                "color": "primary", 
-                "timed": bool(event.get("end_date"))
-            })
+            # if the event is between the calendar boundaries then add to output
+            if event.get("status") == "confirmed":
+                output.append({
+                    "name": event.get("title"), 
+                    "group": event.get("action_network:sponsor", {}).get("title", ""),
+                    "start": arrow.get(event.get("start_date", "")).format("YYYY-MM-DD HH:mm"), 
+                    "embed": get_event_embed(event, headers), 
+                    "end": arrow.get(event.get("end_date", "")).format("YYYY-MM-DD HH:mm") if event.get("end_date") else "", 
+                    "description": event.get("description", ""), 
+                    "featured_image_url": event.get("featured_image_url", ""),
+                    "instructions": event.get("instructions", ""),
+                    "location": event.get('location'),
+                    "uid": event.get("identifiers"),
+                    "link": event.get("browser_url"), 
+                    "color": "primary", 
+                    "timed": bool(event.get("end_date"))
+                })
 
-    # move paging forward
-    current_page += 1
+        # move paging forward
+        current_page += 1
 
 # write out content
 with open('public/events.json', 'w', encoding='utf-8') as f:
